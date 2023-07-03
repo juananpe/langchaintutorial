634f5f986d2c4da767745f76e579e1b3ff76640e
Juanan Pereira <juanan.pereira@ehu.eus>
2023-07-03 16:18:28 +0200

    add some reference URLs to the mpt-7b example

--- 07-1-mpt-7b-test.py
+++ 07-1-mpt-7b-test.py
@@ -16,3 +16,13 @@ retriever = db.as_retriever(search_kwargs={"k": 3})
 qa_chain = RetrievalQA.from_chain_type(llm=llm, 
                                   chain_type="stuff", 
                                   retriever=retriever)
+
+
+# Running LLMs locally
+# https://wandb.ai/capecape/LLMs/reports/A-Guide-on-Running-LLMs-Locally--Vmlldzo0Njg5NzMx
+# Using NEW MPT-7B in Hugging Face and LangChain
+# https://www.youtube.com/watch?v=DXpk9K7DgMo&ab_channel=JamesBriggs
+# Running mpt-30b with a CPU
+# https://github.com/abacaj/mpt-30B-inference
+
+
